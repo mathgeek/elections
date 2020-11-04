12b5d9ed540494a61245441143bd68b31db31e2d
Jaret Dawson <jaret.dawson@gmail.com>
2020-11-04 00:00:32 -0600

    dist again

---
+++ dist/results/results.spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('electionsApp.results module', function() {
+
+  beforeEach(module('electionsApp.results'));
+
+  describe('results controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var resultsCtrl = $controller('resultsCtrl');
+      expect(resultsCtrl).toBeDefined();
+    }));
+
+  });
+});
